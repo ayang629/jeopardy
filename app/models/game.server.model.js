adf21f8abc31f96157b2be0964aa7b50f77842e5
Andrew Yang <akyang1@uci.edu>
2015-04-19 20:32:50 -0700

    testing

---
+++ app/models/game.server.model.js
@@ -0,0 +1,56 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema({
+	question: String, 
+	answer: String, 
+	value: Number
+});
+
+var TopicSchema = new Schema({
+	questions: [QuestionSchema],
+	topicName: String
+});
+
+var PlayerSchema = new Schema({
+	name: String,
+	score: Number
+});
+
+/**
+ * Game Schema
+ */
+var GameSchema = new Schema({
+	name: {
+		type: String,
+		default: '',
+		required: 'Please name your game'
+	},
+	topics: {
+		type: [TopicSchema],
+		default: [],
+		required: 'Please add topics'
+	},
+	players: {
+		type: [PlayerSchema],
+		default: [],
+		required: 'Please add players'
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	}
+});
+mongoose.model('Question', QuestionSchema);
+mongoose.model('Topic', TopicSchema);
+mongoose.model('Player', PlayerSchema);
+mongoose.model('Game', GameSchema);
